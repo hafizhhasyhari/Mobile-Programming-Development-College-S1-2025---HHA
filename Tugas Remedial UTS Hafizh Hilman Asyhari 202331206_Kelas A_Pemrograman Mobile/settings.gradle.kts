72a8158875e1f2048fc5a466d451e60954261ae2
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2025-11-18 22:30:32 +0700

    Add files via upload

---
+++ Tugas Remedial UTS Hafizh Hilman Asyhari 202331206_Kelas A_Pemrograman Mobile/settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// File: settings.gradle.kts
+rootProject.name = "REMEDIAL_UTS_Hafizh Hilman Asyhari" // Ada spasi di sini
+include(":app")
+ 
