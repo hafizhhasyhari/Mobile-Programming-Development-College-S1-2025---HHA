1fbdebb364ff5651125001ffb66968506319afac
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2025-12-17 23:46:11 +0700

    Add files via upload

---
+++ TUGAS 05 CRUD-Hafizh Hilman Asyhari/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "tugas-_5_crud_202331206_hafizh_hilman_asyhari_kelas_a"
+include(":app")
